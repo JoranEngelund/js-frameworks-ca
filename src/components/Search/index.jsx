76b7db121de2de2d47d01157ce5db180af4c8f2c
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2023-06-07 14:30:52 +0200

    --feature-search
    Added:
    
    - Search component for filtering and displaying search results. returns {JSX.Element} Search component JSX
    
    - styled-components for Search component

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,11 +1,35 @@
+import React from "react";
+import { useState } from "react";
+import useApi from "../../hooks/useApi";
+import Loader from "../Loader";
 import * as styled from "../Search/styled";
 
 /**
- * Search component that returns a search form with an input for searching
- * @returns <Search />
+ * Search component for filtering and displaying search results.
+ * @returns {JSX.Element} Search component JSX
  */
 
 export default function Search() {
+  const API_URL = `https://api.noroff.dev/api/v1/online-shop`;
+  const { data, isLoading } = useApi(API_URL);
+
+  const productsData = data;
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const inputQuery = (e) => {
+    const inputValue = e.target.value;
+    setSearchQuery(inputValue);
+  };
+
+  const filteredQuery = productsData.filter((product) => {
+    const productTitle = product.title.trim().toLowerCase();
+    const searchQueryLowerCase = searchQuery.toLowerCase();
+
+    return productTitle.includes(searchQueryLowerCase) && searchQuery !== "";
+  });
+
+  console.log(filteredQuery);
   return (
     <div>
       <styled.SearchForm>
@@ -13,7 +37,26 @@ export default function Search() {
           type="search"
           name="search"
           placeholder="Search..."
+          value={searchQuery}
+          onChange={inputQuery}
         />
+        {filteredQuery.length > 0 && (
+          <styled.ResultContainer>
+            {filteredQuery.map((product) => (
+              <styled.QueryResult
+                key={product.id}
+                href={`/product/${product.id}`}
+              >
+                {isLoading ? <Loader /> : ""}
+                <styled.QueryThumbnail
+                  src={product.imageUrl}
+                  alt={product.title}
+                />
+                <p>{product.title}</p>
+              </styled.QueryResult>
+            ))}
+          </styled.ResultContainer>
+        )}
       </styled.SearchForm>
     </div>
   );
